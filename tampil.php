59ffdb76c11a271a76a9dad3fee1f20c2696bcfa
mirtaayuss <40625442+mirtaayuss@users.noreply.github.com>
2019-04-12 19:39:16 +0700

    Add files via upload

---
+++ tampil.php
@@ -0,0 +1,111 @@
+</title> <style> body { padding-top: 70px; } </style>
+
+</ul>
+			</div>
+        </div>
+	</nav>
+	
+
+	<div class="container">
+		<div class="row">
+				<div class="jumbotron">
+						<h1 class="display-4">Selamat Datang Kaks!</h1>
+						<p>Segalanya Bisa Dijual Dan Mendapatkan Duit</p>
+					  <p>
+						  <a class="btn btn-primary btn-lg" href="pasangiklan.php"> + Pasang Iklan</a>
+					  </p>
+				</div>
+
+				<div class="jumbotron">
+					<form>
+						<div class="form-group">
+							<input type="text" class="form-control" name="cari" placeholder="Tampilan Item...">
+						</div>
+						<button type="submit" class="btn btn-info">Tampil</button>
+					</form>
+				</div>
+
+			<div class="col-lg-12 text-center">
+				<!-- <h1>Coba Bootstrap</h1>
+				<p class="lead">Aplikasi menggunakan Bootstrap</p> -->
+
+				<div class="coutainer">
+						<div class="row">
+							<center><h2>Featured Brands</h2></center><br>
+							<br><br><br>
+						  <div class="col-sm-12 text-center">
+						  </div>
+						</div>
+				
+					<div class="row">
+					  <div class="col-sm-3">
+							<a href="" class="thumbnail">
+							  <img src="gambar/vans.png">
+							</a>
+					  </div>
+					  <div class="col-sm-3">
+							<a href="" class="thumbnail">
+							  <img src="gambar/dickies.jpg">
+							</a>
+					  </div>
+					  <div class="col-sm-3">
+							<a href="" class="thumbnail">
+							  <img src="gambar/emerica.jpg">
+							</a>
+					  </div>
+						<div class="col-sm-3">
+							<a href="" class="thumbnail">
+							  <img src="gambar/converse.jpg">
+							</a>
+					  </div>
+					  <div class="col-sm-3">
+							<a href="" class="thumbnail">
+							  <img src="gambar/bape.jpg">
+							</a>
+					  </div>
+					  <div class="col-sm-3">
+							<a href="" class="thumbnail">
+							  <img src="gambar/puma.png">
+							</a>
+					  </div>
+					  <div class="col-sm-3">
+							<a href="" class="thumbnail">
+							  <img src="gambar/gucci.jpg">
+							</a>
+					  </div>
+						<div class="col-sm-3">
+							<a href="" class="thumbnail">
+							  <img src="gambar/dw.jpg">
+							</a>
+						</div>					
+					</div>
+					</div>	 
+				</div>
+
+			</div>
+	</div>
+</div>
+
+
+<!-- footer -->
+<footer>
+		<div class="countainer text-center">
+		  <div class="row">
+			<div class="col-sm-12">
+			  <p>&copy; 2018 | builth with love <i class="glyphicon glyphicon-globe"></i> by.
+				<a href="https://www.instagram.com/mirtaayuss/?hl=en">Mirta Ayu Sekar Sari</a></p>
+			</div>
+		  </div>
+			<div class="row">
+				<div class="col-sm-12">
+				  <a href="https://www.youtube.com/channel/UCjPeFxJIGHzzZNMZX2HweZQ" class="btn btn-danger"><i class="glyphicon glyphicon-play"></i>
+					Subsribe to youtube</a>
+			<div>
+		  </div>
+	  </footer>
+	  <!-- akhirfooter> -->
+
+	<script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
